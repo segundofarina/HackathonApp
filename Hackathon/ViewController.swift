0f1e66e5d129bcbe80e5e3a5c6dd1855c9f8dbde
Martin <martin@victory.com.ar>
2017-11-04 00:26:07 -0300

    test merge

--- Hackathon/ViewController.swift
+++ Hackathon/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
          var martin = false
         if martin == false{
-            print("hello")
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
