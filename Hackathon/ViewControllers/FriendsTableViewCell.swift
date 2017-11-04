edf2a92c2d7d304396e324fef5c7d8ed42f21620
Martin <martin@victory.com.ar>
2017-11-04 05:30:30 -0300

    agrego friends table view

---
+++ Hackathon/ViewControllers/FriendsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsTableViewCell.swift
+//  Hackathon
+//
+//  Created by Martin Victory on 4/11/17.
+//  Copyright © 2017 Segundo Fariña. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
