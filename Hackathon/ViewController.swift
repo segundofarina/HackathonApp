8927f0f1ee5e67bcf3f550185009a6393586e80a
Segundo Fariña <afarina@itba.edu.ar>
2017-11-04 00:16:27 -0300

    Initial Commit

---
+++ Hackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon
+//
+//  Created by Segundo Fariña on 4/11/17.
+//  Copyright © 2017 Segundo Fariña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
