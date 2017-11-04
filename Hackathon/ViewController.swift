613f2284c6e303fb59a0c55dc607565d3d674717
Segundo Fariña <afarina@itba.edu.ar>
2017-11-04 00:24:38 -0300

    prueba

--- Hackathon/ViewController.swift
+++ Hackathon/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+         var martin = false
+        if martin == false{
+            print("hello")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
